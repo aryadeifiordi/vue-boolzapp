8cb9f31d2a3a017813bd31cfc03357da63fb4cc2
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-14 17:35:11 +0100

    restart

--- MILESTONE 2/asset/js/ main.js
+++ MILESTONE 2/asset/js/ main.js
@@ -1,52 +1,45 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            contacts: [
-                {
-                    id: 1,
-                    name: 'Fabio',
-                    image: './asset/img/avatar_2.jpg',
-                    lastSeen: '16:03',
-                },
-                {
-                    id: 2,
-                    name: 'Samuele',
-                    image: './asset/img/avatar_3.jpg',
-                    lastSeen: '16:03'
-                },
-                {
-                    id: 3,
-                    name: 'Alessandro B.',
-                    image: './asset/img/avatar_3.jpg',
-                    lastSeen: '16:03'
-                },
-                {
-                    id: 4,
-                    name: 'Alessandro L.',
-                    image: './asset/img/avatar_3.jpg',
-                    lastSeen: '16:03'
-                },
-                {
-                    id: 5,
-                    name: 'Claudia',
-                    image: './asset/img/avatar_3.jpg',
-                    lastSeen: '16:03'
-                },
-                {
-                    id: 6,
-                    name: 'Federico',
-                    image: './asset/img/avatar_3.jpg',
-                    lastSeen: '16:03'
-                },
-                {
-                    id: 7,
-                    name: 'Davide',
-                    image: './asset/img/avatar_3.jpg',
-                    lastSeen: '16:03'
+            data() {
+                return {
+                    contacts: [
+                        {
+                            id: 1,
+                            name: 'Fabio',
+                            image: './asset/img/avatar_2.jpg',
+                            lastSeen: '16:03',
+                            messages: [
+                                { text: 'Ciao!', time: '16:05', sender: 'me' },
+                                { text: 'Come stai?', time: '16:07', sender: 'me' },
+                            ]
+                        },
+                        {
+                            id: 2,
+                            name: 'Samuele',
+                            image: './asset/img/avatar_3.jpg',
+                            lastSeen: '16:03',
+                            messages: [
+                                { text: 'Hey!', time: '15:30', sender: 'other' },
+                                { text: 'Tutto bene, grazie!', time: '15:32', sender: 'other' },
+                            ]
+                        },
+                        {
+                            id: 3,
+                            name: 'Marco',
+                            image: './asset/img/avatar_4.jpg',
+                            lastSeen: '16:03',
+                            messages: [
+                                { text: 'Salve!', time: '14:00', sender: 'other' },
+                                { text: 'Come va?', time: '14:05', sender: 'other' },
+                            ]
+                        }
+                    ],
+                    activeContact: null
+                };
+            },
+            methods: {
+                setActiveContact(contact) {
+                    this.activeContact = contact;
                 }
-            ]
-        };
-    }
-});
-
-app.mount('#app');
+            }
+        });
+        app.mount('#app');
