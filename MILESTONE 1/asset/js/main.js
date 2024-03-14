c831c71b869d484374f6aa9e32fba1af455b05c5
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-14 16:25:41 +0100

    milestone 1 done

--- MILESTONE 1/asset/js/main.js
+++ MILESTONE 1/asset/js/main.js
@@ -1,50 +1,52 @@
-console.log('works');
-var app = new Vue({
-    el: '#app',
-    data: {
-        contacts: [
-            {
-                id: 1,
-                name: 'Fabio',
-                image: './asset/img/avatar_2.jpg',
-            },
-            {
-                id: 2,
-                name: 'Samuele',
-                image: './asset/img/avatar_3.jpg',
-                lastSeen: '16:03'
-            },
-            {
-                id: 2,
-                name: 'Alessandro B.',
-                image: './asset/img/avatar_3.jpg',
-                lastSeen: '16:03'
-            },
-            {
-                id: 2,
-                name: 'Alessandro L.',
-                image: './asset/img/avatar_3.jpg',
-                lastSeen: '16:03'
-            },
-            {
-                id: 2,
-                name: 'Claudia',
-                image: './asset/img/avatar_3.jpg',
-                lastSeen: '16:03'
-            },
-            {
-                id: 2,
-                name: 'Federico',
-                image: './asset/img/avatar_3.jpg',
-                lastSeen: '16:03'
-            },
-            {
-                id: 2,
-                name: 'Davide',
-                image: './asset/img/avatar_3.jpg',
-                lastSeen: '16:03'
-            },
-            
-        ]
+const app = Vue.createApp({
+    data() {
+        return {
+            contacts: [
+                {
+                    id: 1,
+                    name: 'Fabio',
+                    image: './asset/img/avatar_2.jpg',
+                    lastSeen: '16:03',
+                },
+                {
+                    id: 2,
+                    name: 'Samuele',
+                    image: './asset/img/avatar_3.jpg',
+                    lastSeen: '16:03'
+                },
+                {
+                    id: 3,
+                    name: 'Alessandro B.',
+                    image: './asset/img/avatar_3.jpg',
+                    lastSeen: '16:03'
+                },
+                {
+                    id: 4,
+                    name: 'Alessandro L.',
+                    image: './asset/img/avatar_3.jpg',
+                    lastSeen: '16:03'
+                },
+                {
+                    id: 5,
+                    name: 'Claudia',
+                    image: './asset/img/avatar_3.jpg',
+                    lastSeen: '16:03'
+                },
+                {
+                    id: 6,
+                    name: 'Federico',
+                    image: './asset/img/avatar_3.jpg',
+                    lastSeen: '16:03'
+                },
+                {
+                    id: 7,
+                    name: 'Davide',
+                    image: './asset/img/avatar_3.jpg',
+                    lastSeen: '16:03'
+                }
+            ]
+        };
     }
 });
+
+app.mount('#app');
