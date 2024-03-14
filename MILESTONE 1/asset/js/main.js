0ce28298aef79fd9df1ff06c41930e135648f7d0
aryadeifiordi <arya.93.ar@gmail.com>
2024-03-14 12:12:33 +0100

    start array

--- MILESTONE 1/asset/js/main.js
+++ MILESTONE 1/asset/js/main.js
@@ -1,1 +1,50 @@
 console.log('works');
+var app = new Vue({
+    el: '#app',
+    data: {
+        contacts: [
+            {
+                id: 1,
+                name: 'Fabio',
+                image: './asset/img/avatar_2.jpg',
+            },
+            {
+                id: 2,
+                name: 'Samuele',
+                image: './asset/img/avatar_3.jpg',
+                lastSeen: '16:03'
+            },
+            {
+                id: 2,
+                name: 'Alessandro B.',
+                image: './asset/img/avatar_3.jpg',
+                lastSeen: '16:03'
+            },
+            {
+                id: 2,
+                name: 'Alessandro L.',
+                image: './asset/img/avatar_3.jpg',
+                lastSeen: '16:03'
+            },
+            {
+                id: 2,
+                name: 'Claudia',
+                image: './asset/img/avatar_3.jpg',
+                lastSeen: '16:03'
+            },
+            {
+                id: 2,
+                name: 'Federico',
+                image: './asset/img/avatar_3.jpg',
+                lastSeen: '16:03'
+            },
+            {
+                id: 2,
+                name: 'Davide',
+                image: './asset/img/avatar_3.jpg',
+                lastSeen: '16:03'
+            },
+            
+        ]
+    }
+});
